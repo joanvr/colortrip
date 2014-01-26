71374133b6df40d207509a1954240d3b22d21128
León Miura <leonmiura@gmail.com>
2014-01-26 14:41:31 +0100

    level 3 and cheats

---
+++ Assets/SkipLevel.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+}
+
+function Update () {
+
+}
+
+function OnGUI()
+{
+	if((Application.loadedLevel > 0))
+	{
+		if(GUI.Button(new Rect(Screen.width - 330,10,300,50),"Cheat: Skip level"))
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
